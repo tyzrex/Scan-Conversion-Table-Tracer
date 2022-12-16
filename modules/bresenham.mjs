ad2adf4d3d836d5af64b5d78d0f47740e7fb5021
tyzrex <sulavbaral58@gmail.com>
2022-12-17 01:35:09 +0545

    algorithm updates for negative dx and dy

--- modules/bresenham.mjs
+++ modules/bresenham.mjs
@@ -1,67 +1,78 @@
-import {drawLine} from './drawline.mjs';
+import { drawLine } from "./drawline.mjs";
 
 const displayProcedure = document.getElementById("display");
-const canvas = document.getElementById("canvas")
+const canvas = document.getElementById("canvas");
 
-export const traceBresenham = (x0, y0, x1, y1,tab = 0) => {
-    let table = {
-        X: [],
-        Y: [],
-        P : [],
-        XY : [],
-    }
-    let dx = x1 - x0;
-    let dy = y1 - y0;
-    let x = x0;
-    let y = y0;
-    let m = dy / dx;
-    let p = 2 * dy - dx;
+export const traceBresenham = (x0, y0, x1, y1, tab = 0) => {
+  let table = {
+    X: [],
+    Y: [],
+    P: [],
+    XY: [],
+  };
+  let m,dx,dy,x,y,p;
+  if (x0 > x1 || y0 > y1) {
+     dx = Math.abs(x1 - x0);
+     dy = Math.abs(y1 - y0);
+  } else {
+     dx = x1 - x0;
+     dy = y1 - y0;
+  }
+  x = x0;
+  y = y0;
+  m = dy / dx;
+  p = 2 * dy - dx;
+  if (m > 0 && m < 1) {
+    while (x < x1 || x > x1) {
+      table.X.push(x);
+      table.Y.push(y);
+      table.P.push(p);
 
-    if(m>0 && m<1){
-
-        while(x<x1){
-            table.X.push(x);
-            table.Y.push(y);
-            table.P.push(p);
-
-            if(p>=0){
-                y++;
-                p = Number(p) + Number(2 * dy) - Number(2 * dx);
-            }
-            else{
-                p = Number(p) + Number(2 * dy);
-            }
-            x++;
-            table.XY.push([x,y]);
+      if (p >= 0) {
+        if (x0 > x1 || y0 > y1 ) {
+          y--;
+        } else {
+          y++;
         }
+        p = Number(p) + Number(2 * dy) - Number(2 * dx);
+      } else {
+        p = Number(p) + Number(2 * dy);
+      }
+      x++;
+      table.XY.push([x, y]);
     }
+  }
 
-    if(m>1){
-        let p = 2 * dx - dy;
-        while(y<y1){
-            table.X.push(x);
-            table.Y.push(y);
-            table.P.push(p);
-            
-            if(p>=0){
-                x++;
-                console.log(p);
-                p = Number(p) + Number(2 * dx) - Number(2 * dy);
-            }
-            else{
-                p = Number(p) + Number(2 * dx);
-            }
+  if (m > 1) {
+    let p = 2 * dx - dy;
+    while (y < y1 || y > y1) {
+      table.X.push(x);
+      table.Y.push(y);
+      table.P.push(p);
+
+      if (p >= 0) {
+          x++;
+        console.log(p);
+        p = Number(p) + Number(2 * dx) - Number(2 * dy);
+      } else {
+        p = Number(p) + Number(2 * dx);
+      }
+      if (x0 > x1 || y0 > y1) {
+        y--;
+      }
+        else {
             y++;
-            table.XY.push([x,y]);
         }
+      table.XY.push([x, y]);
     }
+  }
 
-    if(canvas.getContext){
-        const ctx = canvas.getContext('2d')
-        drawLine(ctx,[x0,y0],[x1,y1],'black',0.5);
-    }
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d");
+    drawLine(ctx, [x0, y0], [x1, y1], "black", 0.5);
+  }
 
-    displayProcedure.innerHTML = `
+  displayProcedure.innerHTML = `
     <div class="flex gap-8 justify-center items-center text-sm md:text-lg font-extralight">
     <div>
     <p>x0 : ${x0}</p>
@@ -76,7 +87,6 @@ export const traceBresenham = (x0, y0, x1, y1,tab = 0) => {
     <p>p = 2dy-dx = ${p}</p>
     </div>
 `;
-    console.log(table);
-    return table;
-
-}
+  console.log(table);
+  return table;
+};
